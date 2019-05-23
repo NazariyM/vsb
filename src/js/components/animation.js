a8c17c68055c2a7c52187b3dc6a78e87736e5b62
Nazar Mokrii <nazarriko@gmail.com>
2019-05-23 16:35:21 +0300

    init

--- src/js/components/animation.js
+++ src/js/components/animation.js
@@ -1,7 +1,25 @@
 import ScrollReveal from 'scrollreveal'
+import { Resp } from '../dev/helpers'
 
 ScrollReveal().reveal('.header__container', { delay: 300 })
-ScrollReveal().reveal('.title_lg', { delay: 400, distance: '-50px', origin: 'right' })
-ScrollReveal().reveal('.popular__list', { delay: 550, distance: '-50px', origin: 'right' })
-ScrollReveal().reveal('.category', { delay: 250, distance: '-50px', origin: 'right' })
-ScrollReveal().reveal('.content__aside-block', { delay: 250, distance: '60px'})
+ScrollReveal().reveal('.title_lg', {
+  delay: 400,
+  distance: '-50px',
+  origin: 'right',
+})
+ScrollReveal().reveal('.popular__list', {
+  delay: 550,
+  distance: '-50px',
+  origin: 'right',
+})
+ScrollReveal().reveal('.category', {
+  delay: 250,
+  distance: '-50px',
+  origin: 'right',
+})
+
+if (!Resp.isMobile)
+  ScrollReveal().reveal('.content__aside-block', {
+    delay: 250,
+    distance: '60px',
+  })
