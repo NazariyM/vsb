4e3bacef314abaa4f28dd51c9729f9481832b88d
Nazar Mokrii <nazarriko@gmail.com>
2019-06-10 11:55:52 +0300

    upd

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -2,72 +2,73 @@ import Swiper from 'swiper'
 import { iOS } from '../dev/helpers'
 
 const popSlider = new Swiper('.js-popular-slider', {
-  slidesPerView: 'auto',
-  speed: 500,
-  loop: true,
-  spaceBetween: 30,
-  freeMode: true,
-  grabCursor: true,
-  autoplay: {
-    delay: 2000,
-  },
-  navigation: {
-    disabledClass: 'is-disabled',
-    nextEl: '.popular__list-btn_next',
-    prevEl: '.popular__list-btn_prev',
-  },
-  breakpoints: {
-    767: {
-      spaceBetween: 7,
-    },
-    1250: {
-      spaceBetween: 12,
-    },
-    1439: {
-      spaceBetween: 20,
-    },
-  },
+   slidesPerView: 'auto',
+   speed: 500,
+   loop: true,
+   spaceBetween: 30,
+   freeMode: true,
+   grabCursor: true,
+   autoplay: {
+      delay: 2000,
+   },
+   navigation: {
+      disabledClass: 'is-disabled',
+      nextEl: '.popular__list-btn_next',
+      prevEl: '.popular__list-btn_prev',
+   },
+   breakpoints: {
+      767: {
+         spaceBetween: 7,
+      },
+      1250: {
+         spaceBetween: 12,
+      },
+      1439: {
+         spaceBetween: 20,
+      },
+   },
 })
 
 popSlider.el.addEventListener(
-  'mouseenter',
-  e => {
-    popSlider.autoplay.stop()
-  },
-  false
+   'mouseenter',
+   e => {
+      popSlider.autoplay.stop()
+   },
+   false
 )
 
 popSlider.el.addEventListener(
-  'mouseleave',
-  e => {
-    popSlider.autoplay.start()
-  },
-  false
+   'mouseleave',
+   e => {
+      popSlider.autoplay.start()
+   },
+   false
 )
 
 const breakpoint = window.matchMedia('(min-width: 767px)')
 let contentSwiper
 
 const breakpointChecker = function() {
-  if (breakpoint.matches === true) {
-    if (contentSwiper !== undefined) contentSwiper.destroy(true, true)
-    return
-  } else if (breakpoint.matches === false) {
-    return initContentSwiper()
-  }
+   if (breakpoint.matches === true) {
+      if (contentSwiper !== undefined) contentSwiper.destroy(true, true)
+      return
+   } else if (breakpoint.matches === false) {
+      return initContentSwiper()
+   }
 }
 
 const initContentSwiper = function() {
-  contentSwiper = new Swiper('.js-content-swiper', {
-    slidesPerView: 'auto',
-    speed: 500,
-    navigation: {
-      disabledClass: 'is-active',
-      nextEl: '.content__nav-btn_next',
-      prevEl: '.content__nav-btn_prev',
-    },
-  })
+   contentSwiper = new Swiper('.js-content-swiper', {
+      slidesPerView: 'auto',
+      speed: 500,
+      navigation: {
+         disabledClass: 'is-active',
+         nextEl: '.content__nav-btn_next',
+         prevEl: '.content__nav-btn_prev',
+      },
+   })
 
+   /*
   if (iOS) {
     const slider = document.querySelector('.js-content-swiper')
     const slides = slider.querySelectorAll('.swiper-slide')
@@ -76,6 +77,7 @@ const initContentSwiper = function() {
       slide.classList.add('swiper-no-swiping')
     })
   }
+  */
 }
 breakpoint.addListener(breakpointChecker)
 
